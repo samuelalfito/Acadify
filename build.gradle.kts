52460027de2f7f9f6ed09dce5f4a31364f1220c7
meouz <samuel.deanova@gmail.com>
2024-11-19 01:54:01 +0700

    chore: version and google

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,8 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
+buildscript {
+    dependencies {
+        classpath(libs.google.services)
+    }
+}
