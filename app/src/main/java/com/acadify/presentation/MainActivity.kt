65e700184bce3b240bb4f0c5c8f802c7bc7531b2
meouz <samuel.deanova@gmail.com>
2024-11-19 01:56:47 +0700

    add: new navigate

--- app/src/main/java/com/acadify/presentation/MainActivity.kt
+++ app/src/main/java/com/acadify/presentation/MainActivity.kt
@@ -3,25 +3,53 @@ package com.acadify.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.acadify.ui.theme.AcadifyTheme
+import com.acadify.presentation.analisisakademik.AnalisisAkademikScreen
+import com.acadify.presentation.auth.login.LoginScreen
+import com.acadify.presentation.auth.register.RegisterScreen
+import com.acadify.presentation.kelolanilai.KelolaNilaiScreen
+import com.acadify.presentation.prediksiip.PrediksiIPScreen
+import com.acadify.presentation.simulasinilaiipk.SimulasiNilaiIPKScreen
+import com.acadify.presentation.ui.theme.AcadifyTheme
+import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        FirebaseApp.initializeApp(this)
         setContent {
             AcadifyTheme {
                 val navController = rememberNavController()
                 
-                NavHost(navController = navController, startDestination = "") {
-                    composable("") {
+                NavHost(navController = navController, startDestination = "splash_screen") {
+                    composable("splash_screen") {
+                        SplashScreen(navController)
                     }
                     
-                    composable("") {
+                    composable("login_screen") {
+                        LoginScreen(navController)
+                    }
+                    
+                    composable("register_screen") {
+                        RegisterScreen(navController)
+                    }
+                    
+                    composable("kelola_nilai") {
+                        KelolaNilaiScreen(navController)
+                    }
+                    
+                    composable("prediksi_ip") {
+                        PrediksiIPScreen(navController)
+                    }
+                    
+                    composable("simulasi_nilai_ipk") {
+                        SimulasiNilaiIPKScreen(navController)
+                    }
+                    
+                    composable("analisis_akademik") {
+                        AnalisisAkademikScreen(navController)
                     }
                 }
             }
