3f78cf94476146739d8b3a85c7b1f43c46f5ba9f
meouz <samuel.deanova@gmail.com>
2024-11-19 01:56:04 +0700

    feat: editNilai

---
+++ app/src/main/java/com/acadify/model/data/MataKuliah.kt
@@ -0,0 +1,7 @@
+package com.acadify.model.data
+
+data class MataKuliah(
+    val nama: String,
+    val nilai: Float,
+    val jumlahSKS: Float,
+)
---
+++ app/src/main/java/com/acadify/presentation/kelolanilai/EditNilaiScreen.kt
@@ -0,0 +1,62 @@
+package com.acadify.presentation.kelolanilai
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EditNilaiScreen() {
+    var nama = remember { mutableStateOf("") }
+    var nilai = remember { mutableStateOf("") }
+    var jumlahSKS = remember { mutableStateOf("") }
+    Card {
+        Column(modifier = Modifier.padding(8.dp)) {
+            OutlinedTextField(
+                value = nama.value,
+                onValueChange = { newValue -> nama.value = newValue },
+                placeholder = { Text("Nama") },
+                modifier = Modifier.padding(bottom = 10.dp),
+                maxLines = 1,
+                singleLine = true
+            )
+            OutlinedTextField(
+                value = nilai.value,
+                onValueChange = { newValue -> nilai.value = newValue },
+                placeholder = { Text("Nilai") },
+                modifier = Modifier.padding(bottom = 10.dp),
+                maxLines = 1,
+                singleLine = true
+            )
+            OutlinedTextField(
+                value = jumlahSKS.value,
+                onValueChange = { newValue -> jumlahSKS.value = newValue },
+                placeholder = { Text("jumlahSKS") },
+                modifier = Modifier.padding(bottom = 10.dp),
+                maxLines = 1,
+                singleLine = true
+            )
+            Button(modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+                onClick = {
+                }) {
+                Text("Save")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun EditNilaiScreenPreview() {
+    EditNilaiScreen()
+}
