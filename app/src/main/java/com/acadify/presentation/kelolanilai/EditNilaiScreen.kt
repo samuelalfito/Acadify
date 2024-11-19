2147a142b73a02135b0d2f031e7d065fa3bb696c
meouz <samuel.deanova@gmail.com>
2024-11-19 23:51:27 +0700

    feat: add editNilai

--- app/src/main/java/com/acadify/presentation/kelolanilai/EditNilaiScreen.kt
+++ app/src/main/java/com/acadify/presentation/kelolanilai/EditNilaiScreen.kt
@@ -2,25 +2,53 @@ package com.acadify.presentation.kelolanilai
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.acadify.R
+import com.acadify.model.data.MataKuliah
+import com.acadify.model.data.TambahNilai
 
 @Composable
-fun EditNilaiScreen() {
-    var nama = remember { mutableStateOf("") }
-    var nilai = remember { mutableStateOf("") }
-    var jumlahSKS = remember { mutableStateOf("") }
+fun EditNilaiScreen(
+    viewModel: KelolaNilaiViewModel,
+    mataKuliah: MataKuliah,
+    isEditNilaiScreenVisible: MutableState<Boolean>,
+) {
+    var nama = remember { mutableStateOf(mataKuliah.tambahNilai.nama) }
+    var nilai = remember { mutableStateOf(mataKuliah.tambahNilai.nilai.toString()) }
+    var jumlahSKS = remember { mutableStateOf(mataKuliah.tambahNilai.jumlahSKS.toString()) }
+    
     Card {
         Column(modifier = Modifier.padding(8.dp)) {
+            Card(
+                modifier = Modifier.align(Alignment.End),
+                shape = CircleShape,
+                colors = CardDefaults.cardColors(containerColor = Color.Red),
+                onClick = {
+                    isEditNilaiScreenVisible.value = false
+                }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_close),
+                    contentDescription = "Close",
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
+            }
             OutlinedTextField(
                 value = nama.value,
                 onValueChange = { newValue -> nama.value = newValue },
@@ -48,6 +76,17 @@ fun EditNilaiScreen() {
             Button(modifier = Modifier
                 .align(Alignment.CenterHorizontally),
                 onClick = {
+                    viewModel.updateMataKuliah(
+                        oldValue = mataKuliah,
+                        newValue = MataKuliah(
+                            TambahNilai(
+                                nama = nama.value,
+                                nilai = nilai.value.toFloat(),
+                                jumlahSKS = jumlahSKS.value.toFloat()
+                            )
+                        )
+                    )
+                    isEditNilaiScreenVisible.value = false
                 }) {
                 Text("Save")
             }
@@ -58,5 +97,5 @@ fun EditNilaiScreen() {
 @Preview
 @Composable
 fun EditNilaiScreenPreview() {
-    EditNilaiScreen()
+//    EditNilaiScreen()
 }
