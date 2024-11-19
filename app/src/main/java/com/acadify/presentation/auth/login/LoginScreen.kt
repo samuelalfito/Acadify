4618b73c0cee7d6f0cd50f736af75514d6df6b3a
meouz <samuel.deanova@gmail.com>
2024-11-19 23:53:23 +0700

    fix: implement new UI

--- app/src/main/java/com/acadify/presentation/auth/login/LoginScreen.kt
+++ app/src/main/java/com/acadify/presentation/auth/login/LoginScreen.kt
@@ -1,7 +1,7 @@
 package com.acadify.presentation.auth.login
 
-import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,24 +13,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.Divider
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,30 +42,45 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.acadify.R
-import com.google.firebase.FirebaseApp
-import com.google.firebase.auth.FirebaseAuth
+import com.acadify.model.repository.network.FireAuth
+import com.acadify.presentation.ui.theme.Green40
+import com.acadify.presentation.ui.theme.NunitoFontFamily
+import com.acadify.presentation.ui.theme.PurpleBlue40
+import com.acadify.presentation.ui.theme.PurpleBlue80
 
 @Composable
 fun LoginScreen(navController: NavController) {
     var email = remember { mutableStateOf("") }
     var password = remember { mutableStateOf("") }
     var passwordVisible = remember { mutableStateOf(false) }
-    val context = LocalContext.current
-    val auth = FirebaseAuth.getInstance()
+    val fireAuth = FireAuth()
     
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.background)
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(PurpleBlue40, PurpleBlue80)
+                )
+            )
     ) {
+        Spacer(modifier = Modifier.height(50.dp))
         Text(
-            text = "Login"
+            text = "Login",
+            color = Color.White,
+            fontSize = 36.sp,
+            fontWeight = FontWeight.ExtraBold,
+            fontFamily = NunitoFontFamily,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
         )
+        Spacer(modifier = Modifier.height(120.dp))
         Card(
             modifier = Modifier
                 .wrapContentHeight()
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp)
+                .border(width = 1.dp, color = Color.White, shape = RoundedCornerShape(16.dp)),
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
         ) {
             Row(modifier = Modifier.height(IntrinsicSize.Min)) {
                 Icon(
@@ -69,22 +88,27 @@ fun LoginScreen(navController: NavController) {
                     contentDescription = "Email Icon",
                     modifier = Modifier
                         .align(Alignment.CenterVertically)
-                        .padding(horizontal = 10.dp)
+                        .padding(start = 10.dp, end = 6.dp)
+                        .size(20.dp)
                 )
-                Divider(
+                VerticalDivider(
                     modifier = Modifier
                         .fillMaxHeight()
-                        .width(1.dp)
                         .padding(vertical = 10.dp),
-                    color = Color.Gray
+                    thickness = 2.dp,
+                    color = Green40
                 )
                 TextField(
                     value = email.value,
                     onValueChange = { newValue -> email.value = newValue },
                     placeholder = { Text("Email") },
                     colors = TextFieldDefaults.colors(
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Color.Black,
                         focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent
                     ),
                     maxLines = 1,
                     singleLine = true
@@ -97,6 +121,8 @@ fun LoginScreen(navController: NavController) {
                 .wrapContentHeight()
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp)
+                .border(width = 1.dp, color = Color.White, shape = RoundedCornerShape(16.dp)),
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
         ) {
             Row(modifier = Modifier.height(IntrinsicSize.Min)) {
                 Icon(
@@ -104,14 +130,15 @@ fun LoginScreen(navController: NavController) {
                     contentDescription = "Email Icon",
                     modifier = Modifier
                         .align(Alignment.CenterVertically)
+                        .size(36.dp)
                         .padding(horizontal = 10.dp)
                 )
-                Divider(
+                VerticalDivider(
                     modifier = Modifier
                         .fillMaxHeight()
-                        .width(1.dp)
                         .padding(vertical = 10.dp),
-                    color = Color.Gray
+                    thickness = 2.dp,
+                    color = Green40
                 )
                 TextField(
                     modifier = Modifier.weight(1f),
@@ -120,49 +147,62 @@ fun LoginScreen(navController: NavController) {
                     placeholder = { Text("Password") },
                     visualTransformation = if (passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
                     colors = TextFieldDefaults.colors(
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Color.Black,
                         focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent
                     ),
                     maxLines = 1,
                     singleLine = true
                 )
-                Icon(
-                    painter = painterResource(id = if (passwordVisible.value) R.drawable.eye_on else R.drawable.eye_off),
+                Icon(painter = painterResource(id = if (passwordVisible.value) R.drawable.eye_on else R.drawable.eye_off),
                     contentDescription = if (passwordVisible.value) "Hide Password" else "Show Password",
                     modifier = Modifier
+                        .size(36.dp)
                         .align(Alignment.CenterVertically)
                         .padding(end = 20.dp)
                         .clickable {
                             passwordVisible.value = !passwordVisible.value
-                        }
-                )
+                        })
             }
         }
-        Spacer(modifier = Modifier.padding(20.dp))
+        Spacer(modifier = Modifier.padding(60.dp))
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             Button(modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp),
+                elevation = ButtonDefaults.buttonElevation(8.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Green40),
                 onClick = {
-                    auth.signInWithEmailAndPassword(email.value, password.value)
-                        .addOnCompleteListener { task ->
-                            if (task.isSuccessful) {
-                                Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
-                                navController.navigate("kelola_nilai")
-                            } else {
-                                Toast.makeText(context, "Login Failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
-                            }
-                        }
+                    if (email.value.isNotEmpty() && password.value.isNotEmpty()) {
+                        fireAuth.signIn(email.value, password.value)
+                        navController.navigate("kelola_nilai")
+                    }
                 }) {
                 Text(
-                    text = "Login"
+                    text = "Login",
+                    fontFamily = NunitoFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.Black,
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(5.dp)
                 )
             }
         }
-        Button(onClick = {navController.navigate("register_screen")}) {
-            Text(
-                text = "Don't have an account? Register"
-            )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 40.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Don't have an account? Register",
+                color = Color.White,
+                modifier = Modifier.clickable {
+                    navController.navigate("register_screen")
+                })
         }
     }
 }
