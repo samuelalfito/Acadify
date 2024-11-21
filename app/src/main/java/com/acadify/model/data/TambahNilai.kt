94c1342407bdb79b9ac91cdd7bb94b8b1c6f96c0
meouz <samuel.deanova@gmail.com>
2024-11-21 16:44:56 +0700

    fix: TambahNilai data

--- app/src/main/java/com/acadify/model/data/TambahNilai.kt
+++ app/src/main/java/com/acadify/model/data/TambahNilai.kt
@@ -1,7 +1,10 @@
 package com.acadify.model.data
 
 data class TambahNilai(
-    val nama: String,
-    val nilai: Float,
-    val jumlahSKS: Float
-)
+    val nama: String = "",
+    val nilai: Float = 0f,
+    val jumlahSKS: Float = 0f
+) {
+    // No-argument constructor for Firestore
+    constructor() : this("", 0f, 0f)
+}
