57b5df28ba204f1a54a212fa14c7f4a56bec5933
meouz <samuel.deanova@gmail.com>
2024-11-21 16:41:54 +0700

    feat: card for simulasi nilai IPK

---
+++ app/src/main/java/com/acadify/presentation/simulasinilaiipk/SimulasiNilaiIPKCard.kt
@@ -0,0 +1,91 @@
+package com.acadify.presentation.simulasinilaiipk
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.acadify.R
+import com.acadify.model.data.MataKuliah
+import com.acadify.model.data.TambahNilai
+import com.acadify.presentation.ui.theme.PurpleBlue40
+
+@Composable
+fun SimulasiNilaiIPKCard(
+    mataKuliah: MataKuliah,
+    onEditClick: ((MataKuliah) -> Unit)? = null,
+    onDeleteClick: ((MataKuliah) -> Unit)? = null,
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        colors = CardDefaults.cardColors(PurpleBlue40),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Card(
+                    colors = CardDefaults.cardColors(MaterialTheme.colorScheme.background)
+                ) {
+                    Text(
+                        text = mataKuliah.tambahNilai.nama, modifier = Modifier.padding(8.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(text = "Nilai mata kuliah: ${mataKuliah.tambahNilai.nilai}")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(text = "Jumlah SKS: ${mataKuliah.tambahNilai.jumlahSKS}")
+            }
+            if (onEditClick != null && onDeleteClick != null) {
+                Row(
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_settings),
+                        contentDescription = "Edit",
+                        modifier = Modifier
+                            .padding(end = 8.dp)
+                            .clickable {
+                                onEditClick(mataKuliah)
+                            },
+                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_delete),
+                        contentDescription = "Delete",
+                        modifier = Modifier.clickable(onClick = {
+                            onDeleteClick(mataKuliah)
+                        }),
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SimulasiNilaiIPKCardPreview(modifier: Modifier = Modifier) {
+    SimulasiNilaiIPKCard(mataKuliah = MataKuliah(TambahNilai("Matematika", 90f, 3f)),
+        onEditClick = {},
+        onDeleteClick = {})
+}
